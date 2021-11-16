21a46aa01b2f248f588b61498f624f19255e41b8
Arturo Bernal <arturo@iessanvicente.com>
2021-11-16 09:39:59 +0100

    class 16/11

---
+++ Unit 2/Part 1/angular-products/src/app/product-list/product-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../interfaces/product';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  title = "My product's list";
+  headers = {
+    description: 'Product',
+    price: 'Price',
+    available: 'Available',
+    image: 'Image',
+  };
+
+  products: Product[] = [
+    {
+      id: 1,
+      description: 'SSD hard drive',
+      available: '2016-10-03',
+      price: 75,
+      imageUrl: 'assets/ssd.jpg',
+      rating: 5,
+    },
+    {
+      id: 2,
+      description: 'LGA1151 Motherboard',
+      available: '2016-09-15',
+      price: 96.95,
+      imageUrl: 'assets/motherboard.jpg',
+      rating: 4,
+    },
+  ];
+  showImage = true;
+
+  constructor() {}
+
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
+
+}
