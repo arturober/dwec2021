63246e1c030070af9853602cf55451fe1fa7a754
Arturo Bernal <arturo@email.com>
2022-01-11 00:28:57 +0100

    push

--- Unit 3/ionic-products/android/app/capacitor.config.ts
+++ Unit 3/ionic-products/android/app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-products',
+  appId: 'com.arturober.ionicproducts',
+  appName: 'Ionic products',
   webDir: 'www',
   bundledWebRuntime: false
 };
